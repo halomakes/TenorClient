0f928e4aeba0f42ab35d3dbc4d25a4a6f434f3fd
halomademeapc <opr9029@gmail.com>
2020-08-03 19:40:54 -0400

    fill out methods

---
+++ src/Tenor/TenorClient_Media.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tenor.Schema;
+using Tenor.Utils;
+
+namespace Tenor
+{
+    public partial class TenorClient
+    {
+        /// <summary>
+        /// Tell tenor that a GIF has been shared
+        /// </summary>
+        /// <remarks>This helps Tenor's analytics to improve search results</remarks>
+        /// <param name="id">ID of post that was shared</param>
+        /// <param name="query">Query that was used when searching</param>
+        public async Task RecordShareAsync(string id, string query)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentException("ID is required.");
+            }
+
+            var @params = GetParameters(new Dictionary<string, object>
+            {
+                { "q", query },
+                { "id", id }
+            });
+
+            var requestPath = new Uri($"{BaseUrl}v1/autocomplete").ApplyQueryParams(@params);
+
+            await client.GetStringAsync(requestPath.ToString());
+        }
+
+        /// <summary>
+        /// Get a single post by ID
+        /// </summary>
+        /// <param name="id">ID of the post</param>
+        /// <returns>A single post, if found</returns>
+        public async Task<ImagePost> GetPostAsync(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentException("ID is required.");
+            }
+
+            return (await GetPostsAsync(new List<string> { id }, 1))?.Results?.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Get a collection of posts by ID
+        /// </summary>
+        /// <param name="ids">IDs of the posts</param>
+        /// <returns>Matching posts</returns>
+        public Task<GifQueryResult> GetPostsAsync(params string[] ids) => GetPostsAsync(ids);
+
+        /// <summary>
+        /// Get a collection of posts by ID
+        /// </summary>
+        /// <param name="ids">IDs of the posts</param>
+        /// <param name="limit">Maximum # of posts to fetch</param>
+        /// <returns>Matching posts</returns>
+        public async Task<GifQueryResult> GetPostsAsync(IEnumerable<string> ids, int? limit)
+        {
+            var filteredIds = ids.Select(i => i?.Trim()).Where(i => !string.IsNullOrEmpty(i));
+
+            if (!filteredIds.Any())
+            {
+                throw new ArgumentException("At least 1 ID must be provided.");
+            }
+
+            var idString = string.Join(",", filteredIds);
+
+            var @params = GetParameters(new Dictionary<string, object>
+            {
+                { "ids", idString },
+                { "limit", limit }
+            });
+
+            var requestPath = new Uri($"{BaseUrl}v1/gifs").ApplyQueryParams(@params);
+
+            return await client.GetAsync<GifQueryResult>(requestPath.ToString());
+        }
+    }
+}
--- src/Tenor/TenorClient_Search.cs
+++ src/Tenor/TenorClient_Search.cs
@@ -59,5 +59,31 @@ namespace Tenor
 
             return await client.GetAsync<SearchResult>(requestPath.ToString());
         }
+
+        /// <summary>
+        /// Get random GIFs matching a search query
+        /// </summary>
+        /// <param name="query">Query to search on</param>
+        /// <param name="limit">Maximum number of posts to retrieve</param>
+        /// <param name="position">Position to start search from when using pagination</param>
+        /// <returns>Result set</returns>
+        public async Task<GifQueryResult> GetRandomPostsAsync(string query, int? limit = null, string position = null)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                throw new ArgumentException("Query is required.");
+            }
+
+            var @params = GetParameters(new Dictionary<string, object>
+            {
+                { "q", query },
+                { "limit", limit },
+                { "pos", position }
+            });
+
+            var requestPath = new Uri($"{BaseUrl}v1/random").ApplyQueryParams(@params);
+
+            return await client.GetAsync<GifQueryResult>(requestPath.ToString());
+        }
     }
 }
